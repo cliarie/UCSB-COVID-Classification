be55bedb56cc6ad120c0e6c514a688696600ac27
Claire Chen <73154291+cliarie@users.noreply.github.com>
2022-07-25 00:44:19 -0700

    Delete scripts directory

--- scripts/unzip.py
+++
@@ -1,7 +0,0 @@
-import tarfile, glob
-base_dir = '/home/claire/data/seg/COVID/'
-    
-for name in glob.glob(base_dir + '*.gz'):
-     print(name)
-     tf = tarfile.open(name)
-     tf.extractall(base_dir)
